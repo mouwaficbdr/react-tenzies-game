32365648a4e3b3248d670306651b7bfeb2dd4736
Mouwafic Badarou <badaroumouwafic@gmail.com>
2024-08-12 19:45:01 +0100

    +Dice

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,37 @@
+import { useState } from "react"
 import Die from "./components/Die"
 
 export default function App() {
+
+  const [dice, setDice] = useState(allNewDice())
+
+  function allNewDice() {
+    let diceNumbers = []
+
+    for (let i = 0; i < 10; i++) {
+      const randomNumber = Math.floor(Math.random() * 6) + 1
+      diceNumbers.push({
+        value: randomNumber,
+        isHeld: false
+      })
+    }
+    return diceNumbers
+  }
+
+  function rollDice() {
+    setDice(allNewDice())
+  }
+
+  const diceElements = dice.map((die) => {
+    return <Die value={die.value} />;
+  });
+
   return (
     <main>
-      <div className="die-container">
-        <Die value="1"/>
-        <Die value="1"/>
-        <Die value="1"/>
-        <Die value="1"/>
-        <Die value="1"/>
-        <Die value="1"/>
-        <Die value="1"/>
-        <Die value="1"/>
-        <Die value="1"/>
-        <Die value="1"/>
+      <div className="dice-container">
+        {diceElements}
       </div>
+      <button type="button" onClick={rollDice}>Roll</button>
     </main>
   )
 }
