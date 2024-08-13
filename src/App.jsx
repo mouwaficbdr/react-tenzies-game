c046120d9268b70d09fa8bf5f480b0758b22f9ed
Mouwafic Badarou <badaroumouwafic@gmail.com>
2024-08-13 15:05:38 +0100

    +Win condition

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,71 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
+import { nanoid } from "nanoid"
 import Die from "./components/Die"
 
 export default function App() {
 
   const [dice, setDice] = useState(allNewDice())
+  const [tenzies, setTenzies] = useState(false)
+
+  useEffect(() => {
+    const areAllHeld = dice.every(die => die.isHeld)
+    const areAllEqual = dice.every(die => die.value === dice[0].value)
+    if (areAllHeld && areAllEqual) {
+      console.log("You won!")
+    }
+  }, [dice])
+
+  function generateNewDie() {
+    const randomNumber = Math.floor(Math.random() * 6) + 1;
+    return {
+      value: randomNumber,
+      isHeld: false,
+      id: nanoid(),
+    };
+  }
 
   function allNewDice() {
     let diceNumbers = []
-
     for (let i = 0; i < 10; i++) {
-      const randomNumber = Math.floor(Math.random() * 6) + 1
-      diceNumbers.push({
-        value: randomNumber,
-        isHeld: false
-      })
+      diceNumbers.push(generateNewDie())
     }
     return diceNumbers
   }
 
   function rollDice() {
-    setDice(allNewDice())
+    setDice(prevDice =>
+      prevDice.map(die =>
+        die.isHeld ? die
+          :
+        generateNewDie()
+      )
+    )
+  }
+
+  function holdDice(id) {
+    setDice((prevDice) =>
+      prevDice.map((die) =>
+        die.id === id
+          ? {
+              ...die,
+              isHeld: !die.isHeld,
+            }
+          : die
+      )
+    );
   }
 
   const diceElements = dice.map((die) => {
-    return <Die value={die.value} />;
+
+    return <Die key={die.id} value={die.value} isHeld={die.isHeld} holdDice={holdDice} id={die.id}/>;
   });
 
   return (
     <main>
+      <div className="main--text">
+        <h1 className="title">Tenzies</h1>
+        <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+      </div>
       <div className="dice-container">
         {diceElements}
       </div>
--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -1,8 +1,11 @@
 /* eslint-disable react/prop-types */
 
 export default function Die(props) {
+  const dieStyle = {
+    backgroundColor: props.isHeld ? '#59E391' : '',
+  };
   return (
-    <div className="die-el">
+    <div className="die-el" style={dieStyle} onClick={()=>props.holdDice(props.id)}>
       {props.value}
     </div>
   )
